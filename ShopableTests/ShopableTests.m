b2ee6452ccf292d9826d9ab0a99719a84a9520a8
Dan Fairaizl <dan@basicallybits.com>
2012-02-11 17:51:41 -0700

    Implemented basic unit testing

--- ShopableTests/ShopableTests.m
+++ ShopableTests/ShopableTests.m
@@ -8,6 +8,8 @@
 
 #import "ShopableTests.h"
 
+#import "BBStorageManager.h"
+
 @implementation ShopableTests
 
 - (void)setUp
@@ -24,9 +26,23 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testStoreExists
 {
-    STFail(@"Unit tests are not implemented yet in ShopableTests");
+    STAssertTrue([[BBStorageManager sharedManager] storeExists] == YES, @"Database exists");
+}
+
+- (void)testDefaultStores
+{
+    NSArray *stores = [BBStore stores];
+    
+    STAssertTrue([stores count] == 2, @"Two stores exist by default");
+    
+    //grocery store
+    BBStore *groceryStore = [stores objectAtIndex:0];
+
+    STAssertTrue([groceryStore.name isEqualToString:@"Grocery Store"], @"Grocery store name");
+    STAssertTrue([groceryStore.order intValue] == 0 , @"Grocery store is first in display order");
+    STAssertTrue([groceryStore.type intValue] == bbStoreTypeGrocery, @"Grocery store is of correct type");
 }
 
 @end
