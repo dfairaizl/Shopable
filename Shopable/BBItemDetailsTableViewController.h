c82292f70846a56c9a0d861f1c3f15929c7776e1
Dan Fairaizl <dan@basicallybits.com>
2012-06-09 13:06:43 -0600

    Hooked up item details quantity and units

--- Shopable/BBItemDetailsTableViewController.h
+++ Shopable/BBItemDetailsTableViewController.h
@@ -8,10 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+#define bbPickerNumberOfComponents  2
+#define bbPickerComponentQuantity   0
+#define bbPickerComponentUnits      1
+
 @class BBShoppingItem;
 
-@interface BBItemDetailsTableViewController : UITableViewController
+@interface BBItemDetailsTableViewController : UITableViewController <UITableViewDelegate, UITextFieldDelegate,
+                                                                    UIPickerViewDataSource, UIPickerViewDelegate>
 
 @property (weak, nonatomic) BBShoppingItem *currentItem;
+
+@property (strong, nonatomic) IBOutlet UITextField *quantityUnitsTextField;
+@property (strong, nonatomic) IBOutlet UIPickerView *quantityUnitsPicker;
+@property (strong, nonatomic) IBOutlet UIToolbar *pickerToolBar;
+
+- (IBAction)doneButtonPressed:(id)sender;
 
 @end
--- Shopable/BBItemDetailsTableViewController.m
+++ Shopable/BBItemDetailsTableViewController.m
@@ -13,11 +13,30 @@
 
 @interface BBItemDetailsTableViewController ()
 
+//Private Properties
+@property (strong, nonatomic) NSDictionary *quantitiesUnitsPList;
+@property (strong, nonatomic) NSArray *quantities;
+@property (strong, nonatomic) NSArray *units;
+
+//Private Methods
+- (void)updateQuantityAndUnits;
+
 @end
 
-@implementation BBItemDetailsTableViewController
+@implementation BBItemDetailsTableViewController {
+    
+    __weak UITextField *currentTextField;
+    
+    NSInteger quantityRow;
+    NSInteger unitsRow;
+}
 
 @synthesize currentItem;
+@synthesize quantityUnitsTextField;
+@synthesize quantityUnitsPicker;
+@synthesize pickerToolBar;
+
+@synthesize quantitiesUnitsPList, quantities, units;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -48,6 +67,9 @@
 
 - (void)viewDidUnload
 {
+    [self setQuantityUnitsPicker:nil];
+    [self setPickerToolBar:nil];
+    [self setQuantityUnitsTextField:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -58,11 +80,133 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+#pragma mark - IB Actions
+
+- (IBAction)doneButtonPressed:(id)sender {
+    
+    [currentTextField resignFirstResponder];
+}
+
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
 
+}
+
+#pragma mark - UITextFieldDelegate Methods
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    
+    if(textField == self.quantityUnitsTextField) {
+        
+        if(self.quantitiesUnitsPList == nil) {
+            self.quantitiesUnitsPList = [NSDictionary dictionaryWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"QuantityUnits" withExtension:@"plist"]];
+        }
+        
+        self.quantities = [NSArray arrayWithArray:[self.quantitiesUnitsPList objectForKey:@"Quantities"]];
+        
+        //load units based on prior selection (if any)
+        if([self.currentItem.quantity isEqualToString:@"1"] == YES || self.currentItem.quantity == nil) {
+            self.units = [NSArray arrayWithArray:[self.quantitiesUnitsPList objectForKey:@"UnitsSingular"]];
+        }
+        else {
+            self.units = [NSArray arrayWithArray:[self.quantitiesUnitsPList objectForKey:@"UnitsPlural"]];
+        }
+        
+        if([self.quantityUnitsTextField.text length] <= 0) {
+ 
+            self.quantityUnitsTextField.text = [NSString stringWithFormat:@"%@", [self.quantities objectAtIndex:0]];
+            self.currentItem.quantity = [self.quantities objectAtIndex:0];
+        }
+        
+        textField.inputView = self.quantityUnitsPicker;
+        textField.inputAccessoryView = self.pickerToolBar;
+        
+        currentTextField = textField;
+    }
+    
+    return YES;
+}
+
+#pragma mark - UIPickerDataSource Methods
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    
+    return bbPickerNumberOfComponents;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    
+    NSInteger count = 0;
+    
+    if(component == bbPickerComponentQuantity) {
+        count = [self.quantities count];
+    }
+    else if(component == bbPickerComponentUnits) {
+        count = [self.units count];
+    }
+    
+    return count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    
+    NSString *title = nil;
+    
+    if(component == bbPickerComponentQuantity) {
+        title = [self.quantities objectAtIndex:row];
+    }
+    else if(component == bbPickerComponentUnits) {
+        title = [self.units objectAtIndex:row];
+    }
+    
+    return title;
+}
+
+#pragma mark - UIPickerDelegate Methods
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    
+    if(component == bbPickerComponentQuantity) {
+        
+        //check if quantity selected was greater than 1
+        if([[self.quantities objectAtIndex:row] isEqualToString:@"1"] == NO) {
+            
+            self.units = [NSArray arrayWithArray:[quantitiesUnitsPList objectForKey:@"UnitsPlural"]];
+            
+            [self.quantityUnitsPicker reloadComponent:bbPickerComponentUnits];
+        }
+        else {
+            
+            self.units = [NSArray arrayWithArray:[self.quantitiesUnitsPList objectForKey:@"UnitsSingular"]];
+            
+            [self.quantityUnitsPicker reloadComponent:bbPickerComponentUnits];
+        }
+        
+        quantityRow = row;
+    }
+    else if(component == bbPickerComponentUnits) {
+        
+        unitsRow = row;
+    }
+    
+    [self updateQuantityAndUnits];
+}
+
+#pragma mark - Private Methods
+
+- (void)updateQuantityAndUnits {
+    
+    NSString *quantity = [self pickerView:self.quantityUnitsPicker titleForRow:quantityRow forComponent:bbPickerComponentQuantity];
+    NSString *unit = [self pickerView:self.quantityUnitsPicker titleForRow:unitsRow forComponent:bbPickerComponentUnits];
+    
+    if([quantity length] || [unit length]) {
+        self.quantityUnitsTextField.text = [NSString stringWithFormat:@"%@ %@", quantity, unit];
+    }
+    
+    self.currentItem.quantity = quantity;
+    self.currentItem.units = unit;
 }
 
 @end
